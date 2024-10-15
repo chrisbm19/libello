c37fb83f45e4542118629d141c11e3c3749f317f
chrisbm19 <christopher.mitchell.business@gmail.com>
2024-10-15 02:30:35 +0000

    initial button test

--- libello/app.py
+++ libello/app.py
@@ -1,6 +1,9 @@
 from nicegui import ui
+from dice import *
 
-ui.label('Hello NiceGUI!')
-ui.button('BUTTON', on_click=lambda: ui.notify('button was pressed'))
+ui.label('Initial Button Testing')
+with ui.button(on_click=lambda: ui.notify(f"Result: {d20()}")):
+    ui.label("D20")
+    ui.image("https://cdn-icons-png.flaticon.com/512/6729/6729800.png")
 
 ui.run()
